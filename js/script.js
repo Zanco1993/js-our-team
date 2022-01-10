a2512cd482aa5db10d217e3005254c57317bef6c
Zanco1993 <zanconatofabio@gmail.com>
2022-01-10 16:10:28 +0100

    concluso l'esercizio con innerHtml

--- js/script.js
+++ js/script.js
@@ -30,4 +30,58 @@ const team = [
       image: 'barbara-ramos-graphic-designer.jpg',
     },
   ];
+
+
+/*
+ Viene fornito un layout di base e un file js in cui è definito un array di oggetti che rappresentano i membro del team.
+ Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+ Prendendo come riferimento la card di esempio presente nell’html, stampiamo dinamicamente una card per ogni membro del team.
+*/
   
+// richiamo il mio container per poi aggiugere i vari membri del team
+
+const teamContainer = document.querySelector(".team-container");
+const teamCard = document.querySelector(".team-card");
+const cardImage = document.querySelector(".card-image");
+
+// attraverso un ciclo for, "cammino" nella lista per quanti sono gli elementi
+
+// primo metodo non concluso
+
+// for(let i = 0; i < team.length; i++) {
+
+//   const name = document.createElement("div");
+//   name.className = 'team-card';
+//   teamContainer.append(name);
+
+//   const card = document.createElement("div");
+//   card.className = 'card-image';
+//   teamCard.append(card);
+
+//   const image = document.createElement("img");
+//   image.src = team[i].image;
+//   cardImage.append(image);
+
+// }
+
+
+// secondo metodo innerHtml
+for(let i = 0; i < team.length; i++) {
+  
+  teamContainer.innerHTML += `
+  <div class="team-card">
+    <div class="card-image">
+      <img
+        src="./img/${team[i+1].image}" 
+        alt="member"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${team[i+1].name}</h3>
+      <p>${team[i+1].role}</p>
+    </div>
+  </div>
+  `
+}
+
+// aggiunto il +1 perchè volevo che rimanesse la carta iniziale già scritta nell'html
